a48555700dc555780fb17b4ad6a119b31b6c3d82
Anish Chamaria <anishgone@gmail.com>
2022-09-25 12:07:12 +0530

    Added portfolio and changed DB

--- Assignments/Team Lead (Anish Chamaria)/Assignment 2/Flask App with DB/app.py
+++ Assignments/Team Lead (Anish Chamaria)/Assignment 2/Flask App with DB/app.py
@@ -1,33 +1,27 @@
-from flask import Flask, render_template, request
-from flask_mysqldb import MySQL
-
+from flask import Flask, render_template, request, redirect, session
+import sqlite3 as sql
 
 app = Flask(__name__)
-
-#Config DB
-# db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '*******'  #enter mysql password
-app.config['MYSQL_DB'] = 'flask_app'
-
-
-mysql = MySQL(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         #fetch user data
-        userDetails = request.form
-        name = userDetails['name']
-        email = userDetails['email']
-        cur = mysql.connection.cursor()
-        print(cur)
-        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
-        mysql.connection.commit()
-        cur.close()
-        return 'Success'
+        studentDetails = request.form
+        name = studentDetails['name']
+        email = studentDetails['email']
+
+        try:
+            with sql.connect("student_database.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO students (name,email) VALUES (?,?)",(name,email) )
+                con.commit()
+                msg = "Record successfully added!"
+        except:
+            con.rollback()
+            msg = "Error in INSERT Operation"
+        finally:
+            return msg;
     return render_template('index.html')
 
 if __name__ == '__main__':
