0d9ff22e0185b639af536afe1acc50fc7caa8c07
Anish Chamaria <anishgone@gmail.com>
2022-11-06 20:37:37 +0530

    Assignment 2 completed

--- Assignments/Team Lead (Anish Chamaria)/Assignment 2/Sign In & Sign Up/app.py
+++ Assignments/Team Lead (Anish Chamaria)/Assignment 2/Sign In & Sign Up/app.py
@@ -60,5 +60,9 @@ def login():
             return msg;
     return render_template('login.html')
 
+@app.route('/homepage', methods=['GET', 'POST'])
+def homepage():
+    return render_template('homepage.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
